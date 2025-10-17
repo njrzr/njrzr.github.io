96949986d982461b1a1af311a41fbe06c0b6da97
Orlando Pernia <69866332+njrzr@users.noreply.github.com>
2025-10-17 12:30:00 -0400

    Changelog 17/10/2025
    - Adjustments to scroll and padding in about and curriculum views.
    - Deleted classes in skills view.
    - Adjustments to icon position in skills view.

--- pages/about.vue
+++ pages/about.vue
@@ -15,10 +15,10 @@ const { data, status } = await useLazyFetch('/api/getAbout')
   <div class="relative w-full h-full bg-[url('/images/background-about-blurred.jpg')] bg-no-repeat bg-cover bg-center">
     <menubar></menubar>
 
-    <div v-if="status == 'pending' && data == null" class="relative w-full h-full loading bg-no-repeat bg-gradient-to-r from-transparent via-main to-transparent"></div>
+    <div v-if="status == 'pending' && data == null" class="relative w-full h-full loading scrollbar-none bg-no-repeat bg-gradient-to-r from-transparent via-main to-transparent"></div>
 
     <div v-else class="relative flex justify-end items-start w-full h-full">
-      <p v-html="data" class="relative w-full md:w-1/2 h-full p-8 pt-14 md:p-8 bg-white/75 font-semibold text-xl md:text-4xl font-grenze overflow-y-scroll scrollbar-none"></p>
+      <p v-html="data" class="relative w-full md:w-1/2 h-full p-8 pt-14 md:p-16 bg-white/75 font-semibold text-xl md:text-4xl font-grenze overflow-y-scroll scrollbar-none"></p>
     </div>
 
     <p class="absolute left-4 bottom-4 text-4xl sm:text-8xl text-white shadow-text font-grenze">Sobre mi</p>
@@ -32,5 +32,9 @@ const { data, status } = await useLazyFetch('/api/getAbout')
 
 .box-shadow {
   box-shadow: 1rem 1rem 0.25rem #000;
+}
+
+.scrollbar-none {
+  scrollbar-width: none;
 }
 </style>
--- pages/skills.vue
+++ pages/skills.vue
@@ -18,10 +18,20 @@ const yStyle = ref(0)
 const clientWidth = ref(0)
 const clientHeight = ref(0)
 const skill = ref('');
+const timeout = ref(null)
 
 const hoverDescription = (skillType: string = ''): void => {
+  if (timeout.value != null) clearTimeout(timeout.value)
+  
   showDescription.value = !showDescription.value
-  skillType.length > 0 ? skill.value = skillType : skill.value = ''
+  
+  if (skillType == 'leave') {
+    timeout.value = setTimeout(() => {
+      skill.value = ''
+    }, 500)
+  }
+  
+  if (skillType != 'leave') skill.value = skillType
 }
 
 const update = (event: Event) => {
@@ -63,42 +73,42 @@ onUnmounted(() => {
     <div v-if="status == 'pending' && data == null" class="relative w-full h-full loading bg-no-repeat bg-gradient-to-r from-transparent via-main to-transparent"></div>
 
     <div v-else class="relative md:flex p-2 pb-16 md:p-0 md:pb-0 md:justify-center md:items-center w-full h-full overflow-y-scroll md:overflow-hidden">
-      <div @mouseenter="hoverDescription('html')" @mouseleave="hoverDescription()" class="absolute hidden md:flex justify-center items-center w-30 h-24 sm:scale-150 bg-main hexagon-calc cursor-pointer translate-calc">
+      <div @mouseenter="hoverDescription('html')" @mouseleave="hoverDescription('leave')" class="absolute hidden md:flex justify-center items-center w-30 h-24 scale-200 bg-main hexagon-calc cursor-pointer translate-calc">
         <img class="relative w-14 h-14 object-cover object-center" src="/assets/svg/html5.svg" alt="HTML5 Icon">
       </div>
 
-      <div class="translate-x-8 sm:translate-x-[20vw] sm:-translate-y-[50vh]">
-        <div @mouseenter="hoverDescription('css')" @mouseleave="hoverDescription()" class="absolute hidden md:flex justify-center items-center w-30 h-24 sm:scale-150 bg-main hexagon-calc cursor-pointer translate-calc">
+      <div class="hidden md:block translate-x-[24vw] -translate-y-[18vh]">
+        <div @mouseenter="hoverDescription('css')" @mouseleave="hoverDescription('leave')" class="absolute flex justify-center items-center w-30 h-24 scale-200 bg-main hexagon-calc cursor-pointer translate-calc">
           <img class="relative w-14 h-14 object-cover object-center" src="/assets/svg/css.svg" alt="CSS Icon">
         </div>
       </div>
 
-      <div class="-translate-x-42 sm:-translate-x-[30vw] translate-y-16 sm:-translate-y-[25vh]">
-        <div @mouseenter="hoverDescription('javascript')" @mouseleave="hoverDescription()" class="absolute hidden md:flex justify-center items-center w-30 h-24 sm:scale-150 bg-main hexagon-calc cursor-pointer translate-calc">
+      <div class="hidden md:block -translate-x-[26vw] -translate-y-[28vh]">
+        <div @mouseenter="hoverDescription('javascript')" @mouseleave="hoverDescription('leave')" class="absolute flex justify-center items-center w-30 h-24 scale-200 bg-main hexagon-calc cursor-pointer translate-calc">
           <img class="relative w-14 h-14 object-cover object-center" src="/assets/svg/javascript.svg" alt="Javscript Icon">
         </div>
       </div>
 
-      <div class="sm:-translate-x-[50vw] -translate-y-48 sm:translate-y-[40vh]">
-        <div @mouseenter="hoverDescription('php')" @mouseleave="hoverDescription()" class="absolute hidden md:flex justify-center items-center w-30 h-24 sm:scale-150 bg-main hexagon-calc cursor-pointer translate-calc">
+      <div class="hidden md:block -translate-x-[30vw] translate-y-[15vh]">
+        <div @mouseenter="hoverDescription('php')" @mouseleave="hoverDescription('leave')" class="absolute flex justify-center items-center w-30 h-24 scale-200 bg-main hexagon-calc cursor-pointer translate-calc">
           <img class="relative w-14 h-14 object-cover object-center" src="/assets/svg/php.svg" alt="PHP Icon">
         </div>
       </div>
 
-      <div class="sm:translate-x-[45vw] translate-y-56 sm:translate-y-[20vh]">
-        <div @mouseenter="hoverDescription('sql')" @mouseleave="hoverDescription()" class="absolute hidden md:flex justify-center items-center w-30 h-24 sm:scale-150 bg-main hexagon-calc cursor-pointer translate-calc">
+      <div class="hidden md:block translate-x-[26vw] translate-y-[25vh]">
+        <div @mouseenter="hoverDescription('sql')" @mouseleave="hoverDescription('leave')" class="absolute flex justify-center items-center w-30 h-24 scale-200 bg-main hexagon-calc cursor-pointer translate-calc">
           <img class="relative w-14 h-14 object-cover object-center" src="/assets/svg/mariadb.svg" alt="MariaDB Icon">
         </div>
       </div>
 
-      <div class="-translate-x-24 sm:-translate-x-[20vh] -translate-y-72 sm:translate-y-[50vh]">
-        <div @mouseenter="hoverDescription('bash')" @mouseleave="hoverDescription()" class="absolute hidden md:flex justify-center items-center w-30 h-24 sm:scale-150 bg-main hexagon-calc cursor-pointer translate-calc">
+      <div class="hidden md:block -translate-x-[20vh] translate-y-[40vh]">
+        <div @mouseenter="hoverDescription('bash')" @mouseleave="hoverDescription('leave')" class="absolute flex justify-center items-center w-30 h-24 scale-200 bg-main hexagon-calc cursor-pointer translate-calc">
           <img class="relative w-14 h-14 object-cover object-center" src="/assets/svg/gnometerminal.svg" alt="Terminal Icon">
         </div>
       </div>
 
-      <div class="translate-x-42 sm:translate-x-[70vw] -translate-y-16 sm:translate-y-[50vh]">
-        <div @mouseenter="hoverDescription('others')" @mouseleave="hoverDescription()" class="absolute hidden md:flex justify-center items-center w-30 h-24 sm:scale-150 bg-main hexagon-calc cursor-pointer translate-calc">
+      <div class="hidden md:block translate-x-[70vw] translate-y-[50vh]">
+        <div @mouseenter="hoverDescription('others')" @mouseleave="hoverDescription('leave')" class="absolute flex justify-center items-center w-30 h-24 scale-200 bg-main hexagon-calc cursor-pointer translate-calc">
           <img class="relative w-14 h-14 object-cover object-center" src="/assets/svg/questionicon.svg" alt="Question Icon">
         </div>
       </div>
@@ -114,7 +124,7 @@ onUnmounted(() => {
       <p v-for="item of data[skill]" class="text-white text-2xl uppercase my-1 pl-4 font-medieval bg-main/50 p-1 rounded">{{ item }}</p>
     </div>
     
-    <p class="absolute left-4 bottom-4 text-4xl sm:text-8xl text-white shadow-text font-medieval">Habilidades</p>
+    <p class="absolute left-4 bottom-4 text-4xl text-8xl text-white shadow-text font-medieval">Habilidades</p>
   </div>
 </template>
 
